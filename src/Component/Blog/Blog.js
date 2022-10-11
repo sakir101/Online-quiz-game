719f0af06be60d138a0a4407a54559f5eae01246
sakir101 <sakirhossainf99@gmail.com>
2022-10-11 15:33:10 +0600

    Updated

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -14,31 +14,20 @@ const Blog = () => {
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>How does context api works?</Accordion.Header>
                     <Accordion.Body>
-                        
+                    React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>useRef Hook Overall idea</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                    The useRef Hook allows users to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.
+                    <p><strong>Use of useRef</strong></p>
+                    <p><strong>Does Not Cause Re-renders</strong></p>
+                    If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render.To avoid this, we can use the useRef Hook. useRef() only returns one item. It returns an Object called current. When we initialize useRef we set the initial value: useRef(0).
+                    <p><strong>Accessing DOM Elements</strong></p>
+                    In general, we want to let React handle all DOM manipulation.But there are some instances where useRef can be used without causing issues. In React, we can add a ref attribute to an element to access it directly in the DOM.
+                    <p><strong>Tracking State Changes</strong></p>
+                    The useRef Hook can also be used to keep track of previous state values.This is because we are able to persist useRef values between renders.This time we use a combination of useState, useEffect, and useRef to keep track of the previous state. In the useEffect, we are updating the useRef current value each time the inputValue is updated by entering text into the input field.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
