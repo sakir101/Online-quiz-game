65ae554b14322011f309ac9ad8bcb981b8a2151c
sakir101 <sakirhossainf99@gmail.com>
2022-10-11 06:45:31 +0600

    Cover image include

--- src/Component/Statistics/Statistics.js
+++ src/Component/Statistics/Statistics.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics = () => {
     return (
         <div>
-            <h2>This is statistics</h2>
+            <h2>This is statistic</h2>
         </div>
     );
 };
